e22d1968198e8a233ba07ad677485ce46b6fce07
andra2602 <andra.andruta60@yahoo.com>
2025-06-23 11:17:22 +0300

    adding blazeroise

--- Showtime/Program.cs
+++ Showtime/Program.cs
@@ -1,10 +1,21 @@
 using Showtime.Components;
+using Blazorise;
+using Blazorise.Bootstrap5;
+using Blazorise.Icons.FontAwesome;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services
+    .AddBlazorise(options =>
+    {
+        options.Immediate = true;
+    })
+    .AddBootstrap5Providers()
+    .AddFontAwesomeIcons();
 
 var app = builder.Build();
 
